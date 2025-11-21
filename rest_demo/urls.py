8c69315b8b3ea2c27f71c21884a8fbcaa1a1fe8d
gitcat456 <okothdenzel65@gmail.com>
2025-11-21 07:21:57 +0300

    adds url pattern for updating BOok instance

--- rest_demo/urls.py
+++ rest_demo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookListCreateAPIView
+from .views import BookListCreateAPIView, BookRetrieveUpdateAPIView
 
 urlpatterns = [
-    path('api/books',BookListCreateAPIView.as_view(), name = 'book_list_create')
+    path('api/books/',BookListCreateAPIView.as_view(), name = 'book_list_create'),
+    path('api/books/<int:pk>/update/',BookRetrieveUpdateAPIView.as_view(), name= 'book_item_update'),
 ]
