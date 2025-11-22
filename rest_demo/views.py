94465a904ae4cc3b4d071d06dd19e52ccd2c4e27
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 14:31:35 +0300

    adds query parameters to the Bool list create view

--- rest_demo/views.py
+++ rest_demo/views.py
@@ -6,6 +6,20 @@ class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
+    def get_queryset(self):
+        queryset = self.queryset
+        author_filter = self.request.query_params.get('author')
+        year_filter = self.request.query_params.get('published_date')
+        
+        if author_filter:
+            queryset = queryset.filter(author__icontains=author_filter)
+        
+        if year_filter:
+            queryset = queryset.filter(published_date__year=year_filter)
+        
+        return queryset
+            
+    
 class BookRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
