2dd746d7b388336296921deb8c799b9bb11614b7
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 15:50:49 +0300

    drf routers

--- rest_demo/urls.py
+++ rest_demo/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import BookListCreateAPIView, BookRetrieveUpdateAPIView
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import BookListCreateAPIView, BookRetrieveUpdateAPIView, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('api/books/',BookListCreateAPIView.as_view(), name = 'book_list_create'),
     path('api/books/<int:pk>/update/',BookRetrieveUpdateAPIView.as_view(), name= 'book_item_update'),
+    path('viewset/', include(router.urls))
 ]
