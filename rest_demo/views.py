9f55cd4ce5de69280565c32c89c33b1224d8c1bf
gitcat456 <okothdenzel65@gmail.com>
2025-11-22 15:56:44 +0300

    adds query params to the viewset

--- rest_demo/views.py
+++ rest_demo/views.py
@@ -28,5 +28,13 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects. all()
     serializer_class = BookSerializer
     
+    def get_queryset(self):
+        queryset = self.queryset
+        author_filter = self.request.query_params.get('author', None)
+        if author_filter is not None:
+             queryset = queryset.filter(author__icontains=author_filter)
+        return queryset
+        
+    
     
     
