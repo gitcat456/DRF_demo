0590c24c2bcb2b3bfea8ee2920ab13e1e44ea8aa
gitcat456 <okothdenzel65@gmail.com>
2025-12-02 00:35:54 +0300

    minimalistic implemntaion of viewsets

--- serials/views.py
+++ serials/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Author, Comment, BlogPost
+from .serializers import BlogPostSerializer, CommentSerializer, AuthorSerializer
 
-# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
