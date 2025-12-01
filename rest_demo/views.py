d363f35a40db564c3c330d8b9c69366fd70ff41a
gitcat456 <okothdenzel65@gmail.com>
2025-12-01 23:26:26 +0300

    bug fixes

--- rest_demo/views.py
+++ rest_demo/views.py
@@ -3,11 +3,10 @@ from .models import Book
 from .serializers import BookSerializer
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
     
     def get_queryset(self):
-        queryset = self.queryset
+        queryset = Book.objects.all()
         author_filter = self.request.query_params.get('author')
         year_filter = self.request.query_params.get('published_date')
         
