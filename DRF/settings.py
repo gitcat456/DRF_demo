ca010f0828a32aa1474e79790e2aad144905ea26
gitcat456 <okothdenzel65@gmail.com>
2025-12-01 23:29:55 +0300

    adds serials app in the list of installed apps

--- DRF/settings.py
+++ DRF/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'rest_demo',
+    'serials',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
