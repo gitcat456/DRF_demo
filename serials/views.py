0f7db731e342e92f4f0a55034c221a32c8f39e86
gitcat456 <okothdenzel65@gmail.com>
2025-12-02 21:48:20 +0300

    creates viewsets to perform crud for my operations

--- serials/views.py
+++ serials/views.py
@@ -10,9 +10,19 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+          
     
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    
+    def get_queryset(self):
+        queryset=self.queryset
+        post_filter=self.request.query_params.get('post_id')
+        
+        if post_filter:
+            queryset=queryset.filter(post=post_filter)
+            
+        return queryset
 
 
