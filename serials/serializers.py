9f50dc28caf11a9dc2f3da60b0a6a7e8e90957cc
gitcat456 <okothdenzel65@gmail.com>
2025-12-02 21:49:32 +0300

    implemets custom serializers to meet my api requirements

--- serials/serializers.py
+++ serials/serializers.py
@@ -7,13 +7,24 @@ class AuthorSerializer(serializers.ModelSerializer):
         model = Author
         fields = '__all__'
         
-
 class BlogPostSerializer(serializers.ModelSerializer):
+    #basically create another field called author_name 
+    author_name = serializers.CharField(source='author.name', read_only=True)
+    
+    #overide the author field to return the author name
+      # author = serializers.CharField(source='authors.name', read_only=True)
+   
+   #return the complete author objects ie nesting 
+    author = AuthorSerializer(read_only=True)
+    
     class Meta: 
         model = BlogPost
         fields = '__all__'
+
         
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        depth = 1
+        
